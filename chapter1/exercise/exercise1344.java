0f3f50d00ade23e35193e21cbdbf0c1c1d49449c
杨渊 <yytqmj@vip.qq.com>
2019-12-30 14:05:10 +0800

    暂停1.3的习题练习 继续学习1.4

---
+++ chapter1/exercise/exercise1344.java
@@ -0,0 +1,31 @@
+import edu.princeton.cs.algs4.*;
+import java.util.*;
+
+class Buffer {
+
+    private int m_size;
+    public Buffer() {
+
+    }
+    public void insert(char c) {
+
+    }
+    public char delete() {
+        return '0';
+    }
+    public void left(int k) {
+
+    }
+    public void right(int k ) {
+
+    }
+    public int size() {
+        return m_size;
+    }
+}
+
+public class exercise1344 {
+    public static void main(String[] args) {
+
+    }
+}
